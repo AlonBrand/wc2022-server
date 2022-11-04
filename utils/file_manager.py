79179bb81201f38e10cc586a7e2da8e5c0895e88
Alon Brand <alonbrand01@gmail.com>
2022-11-04 13:26:17 +0200

    add missing files

---
+++ utils/file_manager.py
@@ -0,0 +1,43 @@
+from numpy import true_divide
+from openpyxl import load_workbook
+import pandas as pd
+
+def insert_row(file_name, row_data):
+    try:
+        wb = load_workbook("files/{file_name}.xlsx".format(file_name=file_name))
+        ws = wb.worksheets[0]
+        ws.append(row_data)
+        wb.save("files/{file_name}.xlsx".format(file_name=file_name))
+        return True
+    except:
+        return False
+
+def get_table(file_name):
+    games = []
+    wb = load_workbook("files/{file_name}.xlsx".format(file_name=file_name))
+    ws = wb.worksheets[0]
+    all_rows = list(ws.rows)
+
+    for row in all_rows[1:]:
+        curr_game = []
+        for cell in row:
+            curr_game.append(cell.value)
+        games.append(curr_game)
+
+    return games
+
+def search_in_table(file_name, user_name, user_password):
+    wb = load_workbook("files/{file_name}.xlsx".format(file_name=file_name))
+    ws = wb.worksheets[0]
+    all_rows = list(ws.rows)
+
+    for row in all_rows[1:]:
+        if row[0].value == user_name:
+            if row[1].value == user_password:
+                return 'User connected'
+            elif row[1].value != user_password:
+                return 'Wrong password'
+        else: continue
+
+    return "User not exist!"
+    
