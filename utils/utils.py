c7b80e266c0da43604fc30474a98099b4d7febb2
Alon Brand <alonbrand01@gmail.com>
2022-12-11 19:26:04 +0200

    add Semi Final scores

--- utils/utils.py
+++ utils/utils.py
@@ -63,6 +63,9 @@ def calculate_game_points(game_realA, game_realB, user_scoreA, user_scoreB, game
     if game_status == 'Eighth' or game_status == 'Quarter':
         bull_point = 4
         part_point = 2
+    elif game_status == 'Semi':
+        bull_point = 5
+        part_point = 2
 
     if game_realA == user_scoreA and game_realB == user_scoreB:
         return bull_point
